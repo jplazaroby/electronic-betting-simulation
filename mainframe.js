b39ccd8553fe9f409e4c1492a93b37a9fbc546f9
jplazaroby <jplazaroby@gmail.com>
2014-07-01 18:16:46 +0200

    Create mainframe.js

---
+++ mainframe.js
@@ -0,0 +1,5 @@
+electronic-betting-simulation
+=============================
+
+We have a electronic betting system here still under development
+the goal is to make a computrized game and then have someone put in a bet
